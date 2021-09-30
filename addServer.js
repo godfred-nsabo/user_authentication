251ee45306f35cb92d29e79f3b309598311f449c
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2021-09-30 14:12:12 +0000

    added bcyrpt-salt for hashing password

--- addServer.js
+++ addServer.js
@@ -2,6 +2,7 @@
 
 const express  = require('express');
 const app = express();
+const bcyrpt = require('bcrypt');
 
 app.use(express.json());
 
@@ -11,10 +12,20 @@ app.get('/users', (req, res) => {
     res.json(users)
 });
 
-app.post('/users', (req, res)=>{
-   let user = {name: req.body.name, password:req.body.password}
-   users.push(user);
-   res.status(201).send()
+app.post('/users', async (req, res)=>{
+    try {
+       let salt = await bcyrpt.genSalt();
+       let hashPassword = await bcyrpt.hash(req.body.password, salt); 
+       console.log(salt);
+       console.log(hashPassword);
+       let user = { name: req.body.name, password: hashPassword }
+       users.push(user);
+       res.status(201).send()
+    } catch (error) {
+        res.status(500).send();
+        console.log(error);
+    }
+
 });
 
 app.listen(3000);
